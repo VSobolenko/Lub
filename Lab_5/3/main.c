3c0db9fad21597a3830fa45966a449be8f7f7628
VSobolenko <43419449+VSobolenko@users.noreply.github.com>
2018-11-12 08:27:52 +0300

    Update main.c

--- Lab_5/3/main.c
+++ Lab_5/3/main.c
@@ -3,17 +3,22 @@
 
 void main()
 {
-	int N=3;
+    int N=3;
     int a[N][N],i,j,k,m;
     for(i=0;i<N;i++)
-	for(j=0;j<N;j++){
-		printf("Vvedite element [%d,%d] - ", i+1, j+1);
-		scanf("%i",&a[i][j]);
-}
+    for(j=0;j<N;j++){
+	printf("Vvedite element [%d,%d] - ", i+1, j+1);
+	scanf("%i",&a[i][j]);
+        }
     for (i=0;i<N;i++)
-        for(j=0;j<N;j++)
-        if (a[i][j]<0) for(k=0,m=i;k<N;k++) a[k][m]/=2;
+    for(j=0;j<N;j++)
+    if (a[i][j]<0) 
+	 for(k=0,m=i;k<N;k++) 
+	a[k][m]/=2;
     for (i=0;i<N;i++)
-        for(j=0;j<N;j++) {printf("%i ",a[i][j]); if(j==2) printf("\n");}
+    for(j=0;j<N;j++){
+	printf("%i ",a[i][j]); 
+	if(j==2) printf("\n");
+        }
     return 0;
 }
